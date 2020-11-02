8153d697de60dd46cd019d64d7944cfc11401d56
toksly <nwizutoks@gmail.com>
2020-11-02 08:20:40 -0600

    worked on quiz

---
+++ js/questions.js
@@ -0,0 +1,86 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+      title: "which of the following is a looping structure in JavaScript?",
+      choices: ["All of the above", "For", "While", "Do-while loops"],
+      answer: "All of the above"
+    },
+    {
+      title: "What are the two basic groups of data types in JavaScript?",
+      choices: [
+        "Primitive and Attribute",
+        "Primitive and Reference types",
+        "Reference types and Attribute",
+        "None of the above"
+      ],
+      answer: "Primitive and Reference types"
+    },
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+      answer: "Alerts"
+    },
+    {
+      title: "Boolean operators that can be used in JavaScript include:",
+      choices: [
+        "'And' Operator &&",
+        "'Or' Operator ||",
+        "'Not' Operator !",
+        "All of the above"
+      ],
+      answer: "All of the above"
+    },
+    {
+      title:
+        "Which one of these is not among the three different types of errors in JavaScript?",
+      choices: [
+        "Animation time errors",
+        "Load time errors",
+        "Run time errors",
+        "Logical Errors"
+      ],
+      answer: "Animation time errors"
+    },
+    {
+      title: "What is the data type of variables in JavaScript?",
+      choices: [
+        "Object data types",
+        "Function data type",
+        "None of the above",
+        "All of the above"
+      ],
+      answer: "Object data types"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
+      answer: "Parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ____.",
+      choices: [
+        "Numbers and strings",
+        "Other arrays",
+        "Booleans",
+        "All of the above"
+      ],
+      answer: "All of the above"
+    },
+    {
+      title:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["Commas", "Curly brackets", "Quotes", "Parentheses"],
+      answer: "Quotes"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "Terminal / bash", "For loops", "console.log"],
+      answer: "console.log"
+    },
+    {
+      title: "What is the type of Pop up boxes available in JavaScript?:",
+      choices: ["Alert", "Confirm", "Prompt", "All the above"],
+      answer: "All the above"
+    }
+  ];
